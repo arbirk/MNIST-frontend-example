46864a26ba182e998c1e962d4d947e7c2d6a7089
arbirk <arbirk@gmail.com>
2022-08-07 21:24:31 +0200

    Added frontend test of predictions

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Test frontend predictions', () => {
+  it('the prediction should be 1', () => {
     cy.visit('http://localhost:1234')
+    cy.get('.canvas').trigger('mousedown')
+    cy.wait(100)
+    cy.get('.canvas').trigger('mouseup')
+    cy.wait(300)
+    cy.get('canvas').click(140, 30)
+    cy.window().then(win => win.drawLine(140,30,140,250))
+    cy.get('canvas').click(140, 250)
+    cy.get('[id=maxNum]') // 8.
+      .should('contain', '1') 
   })
 })
